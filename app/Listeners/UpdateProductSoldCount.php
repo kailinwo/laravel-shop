ff4efd1a2e3f2bc0d2121e47c1dcb5c67367aef4
kaylin <kailinwong@outlook.com>
2020-03-14 02:45:41 +0000

    完善支付后的逻辑，异步处理订单支付之后，销量和邮件发送的需求

---
+++ app/Listeners/UpdateProductSoldCount.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use App\Models\OrderItem;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class UpdateProductSoldCount implements ShouldQueue
+{
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderPaid $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        //从事件的对象中取出对应的订单；
+        $order = $event->getOrder();
+        //预加载商品数据
+        $order->load(['items.product']);
+        //循环遍历订单中的商品
+        foreach ($order->items as $item) {
+            $product = $item->product;
+            //计算对应商品的销量
+            $soldCount = OrderItem::query()
+                ->where('product_id', $product->id)
+                ->whereHas('order', function ($query) {
+                    $query->whereNotNull('paid_at');//关联的订单状态是否已经支付
+                })->sum('amount');
+            //更新商品销量
+            $product->update([
+                'sold_count' => $soldCount,
+            ]);
+        }
+    }
+}
