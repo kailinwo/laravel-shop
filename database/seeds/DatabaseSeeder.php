75631b57ce69e4529d61e4eaa484b1d2d4e6e899
kaylin <kailinwong@outlook.com>
2020-03-17 03:02:12 +0000

    类目 Seeder 文件

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersSeeder::class); //用户填充
         $this->call(UserAddressesSeeder::class); //用户地址填充
+        $this->call(CategoriesSeeder::class); //分类列表填充
         $this->call(ProductsSeeder::class);//产品填充
         $this->call(CouponCodesSeeder::class);//优惠券填充
         $this->call(OrdersSeeder::class); //订单填充
