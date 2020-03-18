bad2639f124045870884eb7992805c454fc04c72
kaylin <kailinwong@outlook.com>
2020-03-18 08:49:22 +0000

    同步商品数据到ES

---
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands\Elasticsearch;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class SyncProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'es:sync-products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '将商品数据同步到 ElasticSearch';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //获取es对象
+        $es = app('es');
+        Product::query()
+            ->with(['skus', 'properties'])
+            ->chunkById(100, function ($products) use ($es) {
+                $this->info(sprintf('正在同步 ID 范围为 %s 至 %s 的商品', $products->first()->id, $products->last()->id));
+                //初始化请求体
+                $req = ['body' => []];
+                //遍历商品
+                foreach ($products as $product) {
+                    // 将商品模型转为 Elasticsearch 所用的数组
+                    $data = $product->toESArray();
+
+                    $req['body'][] = [
+                        'index' => [
+                            '_index' => 'products',
+                            '_id' => $data['id']
+                        ],
+                    ];
+
+                    $req['body'][] = $data;
+                }
+                try {
+                    //使用bulk批量创建
+                    $es->bulk($req);
+                } catch (\Exception $e) {
+                    $this->error($e->getMessage());
+                }
+            });
+        $this->info('同步完成');
+    }
+}
